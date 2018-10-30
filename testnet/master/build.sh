272ac269ad8aff1352fb16ad4cc2c55e5dfa020c
Yada Starmain <yada.starmain@gmail.com>
2018-10-30 10:24:18 +0100

    .

--- testnet/master/build.sh
+++ testnet/master/build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+docker network create liberty_net
+
 cd ./firstnode
 docker build . -t firstnode_master
-docker run -d --name firstnode_master firstnode_master
+docker run -d --name firstnode_master --net liberty_net -v ~/liberty_wallet:/var/share/liberty_wallet firstnode_master
 echo "BUILD OF FIRSTNODE COMPLETE, CHECKING RUNNING CONTAINERS:"
 docker ps
 
 cd ../wallet
 docker build . -t wallet_master
-echo "BUILD OF  WALLET COMPLETE, CHECKING RUNNING CONTAINERS:"
-docker run -d -P --name wallet_master --link firstnode_master wallet_master
+echo "BUILD OF WALLET COMPLETE, CHECKING RUNNING CONTAINERS:"
+docker run -d -P --name wallet_master --net liberty_net -v ~/liberty_wallet:/var/share/liberty_wallet wallet_master
 
 # IF YOU WANT TO CONNECT TO ONE OF THE CONTAINERS CLI TO CHECK ON THINGS USE THE FOLLOWING COMMANDS
 # TO DISCONECT FROM THE CONTAINER AFTER YOU CONNECT WITHOUT KILLING IT USE <CTRL-P> <CTRL-Q>
 # docker exec -it wallet_master /bin/bash or docker exec -it firstnode_master /bin/bash
+
+-v ~/liberty_wallet:/var/share/liberty_wallet
--- testnet/master/firstnode/startnode.sh
+++ testnet/master/firstnode/startnode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/local/bin/libertyd &
-/usr/local/bin/cpuminer -a sha256d -O libertyrpc:030a0bbd2cacfc330494e8e7c -o http://localhost:20417
+screen -dmS miner bash -c '/usr/local/bin/cpuminer -a sha256d -O libertyrpc:030a0bbd2cacfc330494e8e7c -o http://localhost:20417'
